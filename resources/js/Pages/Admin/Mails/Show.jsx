e33329c3676302e6aa53ca572fbb7346e2fe9258
Marcelo Braga <marcelobraga.dev@gmail.com>
2023-04-07 22:07:52 -0300

    Atualizacao

--- resources/js/Pages/Admin/Mails/Show.jsx
+++ resources/js/Pages/Admin/Mails/Show.jsx
@@ -1,10 +1,10 @@
 import Layout from "@/Layouts/Admin/Layout";
-import parse from 'html-react-parser';
+// import parse from 'html-react-parser';
 
 export default function Index({email}) {
     return (
         <Layout container voltar={route('admin.emails.index')}>
-            {email.body && parse(email.body)}
+            {/*{email.body && parse(email.body)}*/}
         </Layout>
     )
 }
