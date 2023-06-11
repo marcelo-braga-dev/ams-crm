0541d9946ce18b0278b1114ee90b7ad186dc629d
Marcelo Braga <marcelobraga.dev@gmail.com>
2023-06-11 19:00:55 -0300

    atualizacao lead

--- resources/js/Components/Inputs/ImagePdf.jsx
+++ resources/js/Components/Inputs/ImagePdf.jsx
@@ -1,21 +1,21 @@
-import {Button} from "reactstrap";
-
 export default function ImagePdf({url}) {
 
     if (url && url.split('.').pop() === 'pdf') {
         return (
-            <Button className="mt-2 mb-3" color="danger" size="sm" href={"/storage/" + url}
-                    target="_blank">
-                <i className="fas fa-file-pdf"></i> Abrir PDF
-            </Button>)
+            <a className="btn btn-danger mt-2" href={"/storage/" + url} target="_blank">
+                <i className="fas fa-file-pdf pe-2" style={{fontSize: 25}}></i> Abrir PDF
+            </a>
+        )
     }
 
     if (url) {
         return (
-            <a href={"/storage/" + url} target="_blank" download>
-                <img className={"mb-3 img-thumbnail"} alt="" src={"/storage/" + url} style={{maxHeight: 200}}/>
-            </a>
+            <div className="text-end">
+                <img className="mb-1 img-thumbnail d-block" alt="" src={"/storage/" + url} style={{maxHeight: 200}}/>
+                <a className="btn btn-primary btn-sm mb-0 p-1" href={"/storage/" + url} target="_blank" download>
+                    Baixar
+                </a>
+            </div>
         )
-
     }
 }
