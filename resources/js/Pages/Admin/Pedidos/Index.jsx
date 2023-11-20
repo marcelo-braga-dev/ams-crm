af6a9f6f04728ea2a306e98d93a966a6dc8a901c
Marcelo Braga <marcelobraga.dev@gmail.com>
2023-11-20 05:21:38 -0300

    atualizacao

--- resources/js/Pages/Admin/Pedidos/Index.jsx
+++ resources/js/Pages/Admin/Pedidos/Index.jsx
@@ -315,7 +315,7 @@ export default function Pedidos({
                                 {modelo === 2 || !modelo ? '' :
                                     <td id="td-11" className='shadow-sm' style={{minWidth: 300}}>
                                         {pedidos.acompanhamento.map((dados) => {
-                                            return (<CardAcompanhamento key={dados.id} dados={dados}
+                                            return (<CardEntregue key={dados.id} dados={dados}
                                                                         cor={coresAbas.acompanhamento}/>)
                                         })}
                                     </td>}
