83875f5924a4600e1290aee05597bcd16e561834
Marcelo Braga <marcelobraga.dev@gmail.com>
2023-11-20 05:24:11 -0300

    atualizacao

--- resources/js/Pages/Admin/Pedidos/Cards/Acompanhamento/CardAcompanhamento.jsx
+++ resources/js/Pages/Admin/Pedidos/Cards/Acompanhamento/CardAcompanhamento.jsx
@@ -1,7 +1,7 @@
 
 import CardPedidos from "../CardPedidos";
 import MenuMore from './MenuMore';
-import BtnAvancaStatus from "./BtnAvancaStatus";
+// import BtnAvancaStatus from "./BtnAvancaStatus";
 
 
 
