2a4a0a102d6939fdb8a0ad099c0ca1aa52abb84f
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-01-12 01:01:36 -0300

    atualizacao

--- resources/js/Components/Elementos/Text.jsx
+++ resources/js/Components/Elementos/Text.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 export default function Text({texto = ''}) {
-    return (
+    return texto ?
         <span dangerouslySetInnerHTML={{__html: texto.replace(/\n/g, "<br />")}} />
-    )
+        : ''
+
 }
