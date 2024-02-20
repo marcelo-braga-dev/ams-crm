a4c5deb4f71e98fcd73f4288ce41c9b047df2663
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-02-20 08:27:49 -0300

    atualizacao

--- resources/js/Components/Leads/LeadsDados.jsx
+++ resources/js/Components/Leads/LeadsDados.jsx
@@ -6,8 +6,8 @@ export default function LeadsDados({dados}) {
                 {dados.cliente.cnpj && <span className="d-block"><b>CNPJ:</b> {dados.cliente.cnpj}</span>}
                 {dados.cliente.rg && <span className="d-block"><b>RG:</b> {dados.cliente.rg}</span>}
                 {dados.cliente.cpf && <span className="d-block"><b>CPF:</b> {dados.cliente.cpf}</span>}
-                {!dados.contato.telefones.length > 0 && <span className="d-block"><b>Telefone:</b> {dados.contato.telefone}</span>}
-                {dados.contato.telefones.length > 0 && <span className="d-block"><b>Telefones:</b> {dados.contato.telefones.map(item => item + ', ')}</span>}
+                {!dados?.contato?.telefones?.length > 0 && <span className="d-block"><b>Telefone:</b> {dados.contato.telefone}</span>}
+                {dados?.contato?.telefones?.length > 0 && <span className="d-block"><b>Telefones:</b> {dados.contato.telefones.map(item => item + ', ')}</span>}
                 {dados.contato.email && <span className="d-block"><b>Email:</b> {dados.contato.email}</span>}
                 {dados.contato.atendente && <span className="d-block"><b>Atendente:</b> {dados.contato.atendente}</span>}
                 {dados.cliente.cidade && <span className="d-block"><b>Cidade/Estado:</b> {dados.cliente.cidade} / {dados.cliente.estado}</span>}
