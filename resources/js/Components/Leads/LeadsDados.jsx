1bf720ab0a101578a39b9a930549734da73d097e
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-02-20 00:49:43 -0300

    atualizacao

--- resources/js/Components/Leads/LeadsDados.jsx
+++ resources/js/Components/Leads/LeadsDados.jsx
@@ -3,22 +3,22 @@ export default function LeadsDados({dados}) {
         <div className="row">
             <div className="col mb-2">
                 <span className="d-block"><b>Nome:</b> {dados.cliente.nome ?? dados.cliente.razao_social}</span>
-                <span className="d-block"><b>CNPJ:</b> {dados.cliente.cnpj}</span>
+                {dados.cliente.cnpj && <span className="d-block"><b>CNPJ:</b> {dados.cliente.cnpj}</span>}
                 {dados.cliente.rg && <span className="d-block"><b>RG:</b> {dados.cliente.rg}</span>}
                 {dados.cliente.cpf && <span className="d-block"><b>CPF:</b> {dados.cliente.cpf}</span>}
-                <span className="d-block"><b>Telefone:</b> {dados.contato.telefone}</span>
+                {!dados.contato.telefones.length > 0 && <span className="d-block"><b>Telefone:</b> {dados.contato.telefone}</span>}
                 {dados.contato.telefones.length > 0 && <span className="d-block"><b>Telefones:</b> {dados.contato.telefones.map(item => item + ', ')}</span>}
-                <span className="d-block"><b>Email:</b> {dados.contato.email}</span>
+                {dados.contato.email && <span className="d-block"><b>Email:</b> {dados.contato.email}</span>}
                 {dados.contato.atendente && <span className="d-block"><b>Atendente:</b> {dados.contato.atendente}</span>}
                 {dados.cliente.cidade && <span className="d-block"><b>Cidade/Estado:</b> {dados.cliente.cidade} / {dados.cliente.estado}</span>}
                 {dados.cliente.endereco && <span className="d-block"><b>Endereço:</b> {dados.cliente.endereco}</span>}
                 <span className="d-block"><b>Status:</b> {dados.infos.status}</span>
-                <span className="d-block"><b>Anotações:</b> {dados.infos.anotacoes}</span>
+                {dados.infos.anotacoes && <span className="d-block"><b>Anotações:</b> {dados.infos.anotacoes}</span>}
             </div>
             <div className="col mb-2">
                 <span className="d-block"><b>ID:</b> #{dados.id}</span>
-                <span className="d-block"><b>Meio de Contato:</b> {dados.infos.contato}</span>
-                <span className="d-block"><b>Observações:</b> {dados.infos.status_anotacoes}</span>
+                {dados.infos.contato && <span className="d-block"><b>Meio de Contato:</b> {dados.infos.contato}</span>}
+                {dados.infos.status_anotacoes && <span className="d-block"><b>Observações:</b> {dados.infos.status_anotacoes}</span>}
                 <span className="d-block"><b>Data de Contato:</b> {dados.infos.status_data}</span>
                 <span className="d-block"><b>Data de Cadastro:</b> {dados.infos.data_criacao}</span>
             </div>
