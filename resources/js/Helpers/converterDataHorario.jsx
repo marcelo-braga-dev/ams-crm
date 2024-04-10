5c4601cae2cb55dff072e71100cb13c8fcc221a0
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-04-10 10:26:52 -0300

    rem bug

--- resources/js/Helpers/converterDataHorario.jsx
+++ resources/js/Helpers/converterDataHorario.jsx
@@ -2,14 +2,14 @@ export function converterDataHorario(data) {
     return  new Date(data).toLocaleString()
 }
 
-export default function convertFloatToMoney(valor, precisao = 2) {
+export default function convertFloatToMoney(valor, precisao = 2, tag = false) {
     const res = new Intl.NumberFormat('pt-BR', {
         minimumFractionDigits: precisao, maximumFractionDigits: precisao
     }).format(valor)
 
     if (res === 'NaN') return 0
-
-    return <span className={(valor < 0) ? 'text-danger' : ''}>{res}</span>
+    if (tag) return <span className={(valor < 0) ? 'text-danger' : ''}>{res}</span>
+    return res
 }
 export const convertMoneyFloat = (valor = 0, precisao = 2) => {
     valor = valor.toString()
