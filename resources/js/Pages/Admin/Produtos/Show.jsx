ea85bcfddbc0dc6b18fcccf5f8412b691b94bebb
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-05-08 08:22:16 -0300

    remo bug

--- resources/js/Pages/Admin/Produtos/Show.jsx
+++ resources/js/Pages/Admin/Produtos/Show.jsx
@@ -10,7 +10,7 @@ import ImagePdf from "@/Components/Elementos/ImagePdf";
 import Text from "@/Components/Elementos/Text";
 
 function CustomTabPanel(props) {
-    const {children, value, index, ...other} = props;
+    const { children, value, index, ...other } = props;
 
     return (
         <div
@@ -21,7 +21,7 @@ function CustomTabPanel(props) {
             {...other}
         >
             {value === index && (
-                <Box sx={{p: 3}}>
+                <Box sx={{ p: 3 }}>
                     <Typography>{children}</Typography>
                 </Box>
             )}
@@ -42,7 +42,7 @@ function a11yProps(index) {
     };
 }
 
-export default function ({produto, infos}) {
+export default function ({ produto, infos }) {
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
@@ -53,7 +53,7 @@ export default function ({produto, infos}) {
         <LayoutAdmin titlePage="Informaçoẽs do Produto" menu="produtos">
             <div className="row">
                 <div className="col-auto">
-                    <ImagePdf url={produto.foto}/>
+                    <ImagePdf url={produto.foto} />
                 </div>
                 <div className="col">
                     <div className="row justify-content-between">
@@ -61,7 +61,7 @@ export default function ({produto, infos}) {
                             <h5 className="d-block">{produto.nome}</h5>
                             <small className="d-block">ID: #{produto.id}</small>
                             <span className="d-block"><b>Preço de Venda:</b> R$ {produto.preco_venda}</span>
-                            <span className="d-block"><b>Preço do Forn.:</b> R$ {produto.preco_fornecedor}</span>
+                            {/* <span className="d-block"><b>Preço do Forn.:</b> R$ {produto.preco_fornecedor}</span> */}
                             <span className="d-block"><b>Unidade:</b> {produto.unidade_nome}</span>
                             <span className="d-block"><b>Categoria:</b> {produto.categoria_nome}</span>
                             <span className="d-block"><b>Fornecedor:</b> {produto.fornecedor_nome}</span>
@@ -69,15 +69,15 @@ export default function ({produto, infos}) {
                         </div>
                         <div className="col-auto">
                             <a href={route('admin.produtos-fornecedores.edit', produto.id)}
-                               className="btn btn-primary btn-sm me-2">Editar</a>
+                                className="btn btn-primary btn-sm me-2">Editar</a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="row mt-4">
+            <div className="mt-4 row">
                 <div className="col">
-                    <Box sx={{width: '100%'}}>
-                        <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                    <Box sx={{ width: '100%' }}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                             <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
                                 <Tab label="Descrição" {...a11yProps(0)} />
                                 <Tab label="Para que Serve?" {...a11yProps(1)} />
@@ -88,24 +88,24 @@ export default function ({produto, infos}) {
                             </Tabs>
                         </Box>
                         <CustomTabPanel value={value} index={0}>
-                            <Text texto={produto.descricao}/>
+                            <Text texto={produto.descricao} />
                         </CustomTabPanel>
                         <CustomTabPanel value={value} index={1}>
-                            <Text texto={infos.utilidade}/>
+                            <Text texto={infos.utilidade} />
                         </CustomTabPanel>
                         <CustomTabPanel value={value} index={2}>
-                            <Text texto={infos.modo_usar}/>
+                            <Text texto={infos.modo_usar} />
                         </CustomTabPanel>
                         <CustomTabPanel value={value} index={3}>
-                            <Text texto={infos.vantagens}/>
+                            <Text texto={infos.vantagens} />
                         </CustomTabPanel>
                         <CustomTabPanel value={value} index={4}>
-                            <Text texto={infos.duvidas}/>
+                            <Text texto={infos.duvidas} />
                         </CustomTabPanel>
                         <CustomTabPanel value={value} index={5}>
                             {infos.galeria.map((item) => {
                                 return (
-                                    <ImagePdf url={item}/>
+                                    <ImagePdf url={item} />
                                 )
                             })}
                         </CustomTabPanel>
