e304bb38da3539104a91c918fa7a7993d7399000
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-05-30 22:13:56 -0300

    atualizacao

--- resources/js/Pages/Admin/Dashboard/Vendas/Partials/VendasAnuais.jsx
+++ resources/js/Pages/Admin/Dashboard/Vendas/Partials/VendasAnuais.jsx
@@ -5,14 +5,19 @@ import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 import {useState} from "react";
 
+const meses = {jan: 0, fev: 0, mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0}
+
 export default function VendasAnuais({vendasMetasAnual, vendasAnual, metasEmpresas,}) {
     let metaAnoTotal = 0, metaAnoEmpresaTotal = 0,
         alcancadoAnoTotal = 0, difTotal = 0, difTotalEmpresa = 0,
         margemMetasVendasTotal = 0, margemMetasVendasTotalEmpresa = 0
 
-    const [mesesVisivel, setMesesVisivel] = useState(
-        {jan: 1, fev: 1, mar: 1, abr: 1, mai: 1, jun: 1, jul: 1, ago: 1, set: 1, out: 1, nov: 1, dez: 1}
-    )
+    let metaAnoTotalB = 0, metaAnoEmpresaTotalB = 0,
+        alcancadoAnoTotalB = 0, difTotalB = 0, difTotalEmpresaB = 0,
+        margemMetasVendasTotalB = 0, margemMetasVendasTotalEmpresaB = 0
+
+    const [mesesVisivelA, setMesesVisivelA] = useState(meses)
+    const [mesesVisivelB, setMesesVisivelB] = useState(meses)
 
     return (
         <div className="card card-body mb-4">
@@ -25,6 +30,8 @@ export default function VendasAnuais({vendasMetasAnual, vendasAnual, metasEmpres
                             <thead>
                             <tr>
                                 <td className="text-center">Mẽs</td>
+                                <td className="text-center">A</td>
+                                <td className="text-center">B</td>
                                 <td>Alcançado</td>
                                 <td>Meta Vendedores</td>
                                 <td>Meta do Setor</td>
@@ -42,7 +49,7 @@ export default function VendasAnuais({vendasMetasAnual, vendasAnual, metasEmpres
                                 const difEmpresa = (alcancado && metaEmpresa) ? (alcancado - metaEmpresa) : 0
                                 const margemEmpresa = ((alcancado - metaEmpresa) / metaEmpresa * 100) + 100
 
-                                if (mesesVisivel?.[item.mes]) {
+                                if (mesesVisivelA?.[item.mes]) {
                                     metaAnoTotal += item.total_metas ?? 0
                                     metaAnoEmpresaTotal += metaEmpresa
                                     alcancadoAnoTotal += alcancado ?? 0
@@ -52,24 +59,47 @@ export default function VendasAnuais({vendasMetasAnual, vendasAnual, metasEmpres
                                     margemMetasVendasTotalEmpresa += margemEmpresa > 100 ? margemEmpresa : (-margemEmpresa)
                                 }
 
+                                if (mesesVisivelB?.[item.mes]) {
+                                    metaAnoTotalB += item.total_metas ?? 0
+                                    metaAnoEmpresaTotalB += metaEmpresa
+                                    alcancadoAnoTotalB += alcancado ?? 0
+                                    difTotalB += dif
+                                    difTotalEmpresaB += difEmpresa
+                                    margemMetasVendasTotalB += margem > 100 ? margem : (-margem)
+                                    margemMetasVendasTotalEmpresaB += margemEmpresa > 100 ? margemEmpresa : (-margemEmpresa)
+                                }
+
                                 return (
                                     <tr key={index}>
                                         <td className="text-center">
                                             <b>{(item.mes).toUpperCase()} </b>
-                                            {mesesVisivel?.[item.mes]
-                                                ? <VisibilityOutlinedIcon fontSize="small" className="cursor-pointer" onClick={() => setMesesVisivel({...mesesVisivel, [item.mes]: 0})}/>
-                                                : <VisibilityOffOutlinedIcon fontSize="small" className="cursor-pointer" onClick={() => setMesesVisivel({...mesesVisivel, [item.mes]: 1})}/>}
                                         </td>
-                                        <td>{!!mesesVisivel?.[item.mes] && <span>R$ {convertFloatToMoney(alcancado)}</span>}</td>
-                                        <td>{!!mesesVisivel?.[item.mes] && <span>R$ {convertFloatToMoney(item.total_metas)}</span>}</td>
-                                        <td>{!!mesesVisivel?.[item.mes] && <span>R$ {convertFloatToMoney(metaEmpresa)}</span>}</td>
+                                        <td className="text-center">
+                                            {mesesVisivelA?.[item.mes]
+                                                ? <VisibilityOutlinedIcon fontSize="small" className="cursor-pointer"
+                                                                          onClick={() => setMesesVisivelA({...mesesVisivelA, [item.mes]: 0})}/>
+                                                : <VisibilityOffOutlinedIcon fontSize="small" className="cursor-pointer"
+                                                                             onClick={() => setMesesVisivelA({...mesesVisivelA, [item.mes]: 1})}/>}
+                                        </td>
+                                        <td className="text-center">
+                                            {mesesVisivelB?.[item.mes]
+                                                ? <VisibilityOutlinedIcon
+                                                    fontSize="small" className="cursor-pointer"
+                                                    onClick={() => setMesesVisivelB({...mesesVisivelB, [item.mes]: 0})}/>
+                                                : <VisibilityOffOutlinedIcon
+                                                    fontSize="small" className="cursor-pointer"
+                                                    onClick={() => setMesesVisivelB({...mesesVisivelB, [item.mes]: 1})}/>}
+                                        </td>
+                                        <td>{<span>R$ {convertFloatToMoney(alcancado)}</span>}</td>
+                                        <td>{<span>R$ {convertFloatToMoney(item.total_metas)}</span>}</td>
+                                        <td>{<span>R$ {convertFloatToMoney(metaEmpresa)}</span>}</td>
                                         <td className={dif >= 0 ? 'text-success' : (alcancado > 0 ? 'text-danger' : '')}>
-                                            {!!mesesVisivel?.[item.mes] && <span>
+                                            {<span>
                                             {alcancado > 0 ? <span>R$ {convertFloatToMoney(dif)} ({convertFloatToMoney(margem)}%)</span> : '-'}
                                             </span>}
                                         </td>
                                         <td className={difEmpresa >= 0 ? 'text-success' : (alcancado > 0 ? 'text-danger' : '')}>
-                                            {!!mesesVisivel?.[item.mes] && <span>
+                                            {<span>
                                             {(alcancado > 0 && metaEmpresa > 0)
                                                 ? <span>R$ {convertFloatToMoney(difEmpresa)} ({convertFloatToMoney(margemEmpresa)}%)</span>
                                                 : '-'}
@@ -79,22 +109,75 @@ export default function VendasAnuais({vendasMetasAnual, vendasAnual, metasEmpres
                                 )
                             })}
                             <tr>
-                                <td className="text-center text-dark"><b>TOTAL</b></td>
+                                <td className="text-center text-dark"><b>TOTAL A</b></td>
+                                <td></td>
+                                <td></td>
                                 <td className="text-dark">R$ {convertFloatToMoney(alcancadoAnoTotal)}</td>
                                 <td className="text-dark">R$ {convertFloatToMoney(metaAnoTotal)}</td>
                                 <td className="text-dark">R$ {convertFloatToMoney(metaAnoEmpresaTotal)}</td>
-                                <td className={difTotal >= 0 ? 'text-success' : (alcancadoAnoTotal > 0 ? 'text-danger' : '')}>
-                                    R$ {alcancadoAnoTotal > 0 ? convertFloatToMoney(difTotal) : '0,00'} (
-                                    {convertFloatToMoney(margemMetasVendasTotal + 100)}%)
+                                <td className={difTotal > 0 ? 'text-success' : (alcancadoAnoTotal > 0 ? 'text-danger' : '')}>
+                                    R$ {alcancadoAnoTotal > 0
+                                    ? <span>{convertFloatToMoney(difTotal)} ({convertFloatToMoney(margemMetasVendasTotal + 100)}%)</span>
+                                    : '0,00'}
                                 </td>
-                                <td className={difTotalEmpresa >= 0 ? 'text-success' : (alcancadoAnoTotal > 0 ? 'text-danger' : '')}>
-                                    R$ {alcancadoAnoTotal > 0 ? convertFloatToMoney(difTotalEmpresa) : '0,00'} (
-                                    {convertFloatToMoney(margemMetasVendasTotalEmpresa + 100)}%)
+                                <td className={difTotalEmpresa > 0 ? 'text-success' : (alcancadoAnoTotal > 0 ? 'text-danger' : '')}>
+                                    R$ {alcancadoAnoTotal > 0
+                                    ? <span>{convertFloatToMoney(difTotalEmpresa)} ({convertFloatToMoney(margemMetasVendasTotalEmpresa + 100)}%)</span>
+                                    : '0,00'}
                                 </td>
                             </tr>
+
+                            <tr>
+                                <td className="text-center text-dark"><b>TOTAL B</b></td>
+                                <td></td>
+                                <td></td>
+                                <td className="text-dark">R$ {convertFloatToMoney(alcancadoAnoTotalB)}</td>
+                                <td className="text-dark">R$ {convertFloatToMoney(metaAnoTotalB)}</td>
+                                <td className="text-dark">R$ {convertFloatToMoney(metaAnoEmpresaTotalB)}</td>
+                                <td className={difTotalB > 0 ? 'text-success' : (alcancadoAnoTotalB > 0 ? 'text-danger' : '')}>
+                                    R$ {alcancadoAnoTotalB > 0
+                                    ? <span>{convertFloatToMoney(difTotalB)} ({convertFloatToMoney(margemMetasVendasTotalB + 100)}%)</span>
+                                    : '0,00'}
+                                </td>
+                                <td className={difTotalEmpresaB > 0 ? 'text-success' : (alcancadoAnoTotalB > 0 ? 'text-danger' : '')}>
+                                    R$ {alcancadoAnoTotalB > 0
+                                    ? <span>{convertFloatToMoney(difTotalEmpresaB)} ({convertFloatToMoney(margemMetasVendasTotalEmpresaB + 100)}%)</span>
+                                    : '0,00'}
+                                </td>
+                            </tr>
+                            {[0].map(item => {
+                                const difAB = Math.abs(difTotal) - Math.abs(difTotalB)
+                                const difABEmpresa = Math.abs(difTotalEmpresa) - Math.abs(difTotalEmpresaB)
+                                const difMargemEmprsa = margemMetasVendasTotalEmpresaB - margemMetasVendasTotalEmpresa
+                                const difMargem = margemMetasVendasTotalB - margemMetasVendasTotal
+
+                                return (
+                                    <tr>
+                                        <td className="text-center text-dark"><b>DIF A e B</b></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td className="text-dark">R$ {convertFloatToMoney(alcancadoAnoTotal - alcancadoAnoTotalB)}</td>
+                                        <td className="text-dark">R$ {convertFloatToMoney(metaAnoTotal - metaAnoTotalB)}</td>
+                                        <td className="text-dark">R$ {convertFloatToMoney(metaAnoEmpresaTotal - metaAnoEmpresaTotalB)}</td>
+                                        <td className={difAB > 0 ? 'text-success' : difAB < 0 ? 'text-danger' : ''}>
+                                            R$ {difAB !== 0 ? <span>{convertFloatToMoney(difAB)} ({convertFloatToMoney(difMargem)}%)</span> : '0,00'}
+                                        </td>
+                                        <td className={difABEmpresa > 0 ? 'text-success' : difABEmpresa < 0 ? 'text-danger' : ''}>
+                                            R$ {difABEmpresa !== 0 ? <span>{convertFloatToMoney(difABEmpresa)} ({convertFloatToMoney(difMargemEmprsa)}%)</span> : '0,00'}
+                                        </td>
+                                    </tr>
+                                )
+                            })}
                             </tbody>
                         </table>
-
+                        <small className="d-block">
+                            Meses Selecionados em <b>A</b> = {
+                            Object.keys(mesesVisivelA).map(item => !!mesesVisivelA[item] && <span key={item}>{item.toUpperCase()}, </span>)}
+                        </small>
+                        <small>
+                            Meses Selecionados em <b>B</b> = {
+                            Object.keys(mesesVisivelB).map(item => !!mesesVisivelB[item] && <span key={item}>{item.toUpperCase()}, </span>)}
+                        </small>
                     </div>
                 </div>
             </div>
