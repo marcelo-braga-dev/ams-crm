37a7aded233af50c4ddcbba3e889f015056a0d75
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-08-16 15:40:56 -0300

    atualizacao

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,13 +1,15 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import {Link, usePage} from '@inertiajs/react';
 
 export default function Guest({ children }) {
+    const {app_settings} = usePage().props;
     return (
         <div className="flex"
-            style={{"backgroundColor": '#252525', minHeight: '100vh'}}>
+            style={{"backgroundColor": app_settings.bg_color, minHeight: '100vh'}}>
             <div className="p-3 pt-6 text-center">
                 <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                    <img src={app_settings.logo} style={{maxWidth: 200}}></img>
+                    {/*<ApplicationLogo className="w-20 h-20 fill-current text-gray-500"/>*/}
                 </Link>
             </div>
 
