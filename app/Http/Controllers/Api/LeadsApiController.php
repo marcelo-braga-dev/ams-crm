5eafef07d2c229cff9662f89dec91c63ce92e390
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-08-23 17:05:29 -0300

    api landpage

--- app/Http/Controllers/Api/LeadsApiController.php
+++ app/Http/Controllers/Api/LeadsApiController.php
@@ -11,7 +11,7 @@ class LeadsApiController extends Controller
     public function cadastrar(Request $request)
     {
         $setor = $request->setor ?? 1;;
-        throw new \DomainException($request->fields['cnpj']['type']);
+        throw new \DomainException($request->fields['cnpj']['value']);
         (new Leads())->create($request, 1);
         return response()->json(['status' => 'success'], 200);
     }
