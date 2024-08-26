822849c42dca9954188032d67dede3d807356f15
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-08-26 10:25:19 -0300

    whatsapp

--- resources/js/Pages/Admin/Chats/Whatsapp/Index.jsx
+++ resources/js/Pages/Admin/Chats/Whatsapp/Index.jsx
@@ -1,17 +1,22 @@
 import Layout from "@/Layouts/Layout.jsx";
 import {useEffect} from "react";
+import CardContainer from "@/Components/Cards/CardContainer.jsx";
+import CardBody from "@/Components/Cards/CardBody.jsx";
 
 const Page = ({width, height}) => {
     const url = "https://app-wa.ams360crm.com.br/whaticket.js"
 
     return <Layout titlePage="Whatsapp" menu="chats" submenu="chats-whatsapp">
-        <iframe
-            src={url}
-            width="100%"
+        <CardContainer>
+                <iframe
+                    src={url}
+                    width="100%"
 
-            style={{height: '80vh'}}
-            title="Whaticket"
-        />
+                    style={{height: '80vh'}}
+                    title="Whaticket"
+                />
+        </CardContainer>
+
     </Layout>
 }
 
