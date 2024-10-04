e242091dac86d2e7e5e3838204d6f278f36d63ac
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-10-04 08:13:09 -0300

    update whatsapp v2

--- app/Http/Controllers/Geral/Chats/Whatsapp/WhatsappController.php
+++ app/Http/Controllers/Geral/Chats/Whatsapp/WhatsappController.php
@@ -21,12 +21,12 @@ class WhatsappController extends Controller
 
     public function chaves()
     {
-        $urlFrontend = env('WHATSAPP_FRONTEND');
-        $urlBackend = env('WHATSAPP_BACKEND');
-        $apiKey = env('WHATSAPP_API_TOKEN');
-//        $urlFrontend = "https://whatsapp.ams360crm.com.br";
-//        $urlBackend = "https://api-whatsapp.ams360crm.com.br";
-//        $apiKey = "0e29751e-46d9-4a8a-b279-0ac73cd2d63f";
+//        $urlFrontend = env('WHATSAPP_FRONTEND');
+//        $urlBackend = env('WHATSAPP_BACKEND');
+//        $apiKey = env('WHATSAPP_API_TOKEN');
+        $urlFrontend = "https://whatsapp.ams360crm.com.br";
+        $urlBackend = "https://api-whatsapp.ams360crm.com.br";
+        $apiKey = "0e29751e-46d9-4a8a-b279-0ac73cd2d63f";
 
         $credenciaisUsuario = (new UsuariosWhatsappRepositories())->credenciais();
 
