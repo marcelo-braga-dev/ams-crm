ad1f4e2cba47c8a60318cd542974dae9d37b2731
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-10-08 09:27:36 -0300

    update filtar funil vendas

--- app/Services/Leads/LeadFunilVendasService.php
+++ app/Services/Leads/LeadFunilVendasService.php
@@ -15,6 +15,22 @@ class LeadFunilVendasService
             ->when($usuario, function ($query) use ($usuario) {
                 return $query->where('user_id', $usuario);
             })
+            // Ordenação condicional
+            ->orderByRaw("CASE WHEN status = 'novo' THEN updated_at ELSE NULL END DESC") // Ordena por status_data para "novo"
+            ->orderByRaw("CASE WHEN status != 'novo' THEN contato_data ELSE NULL END ASC") // Ordena por contato_data para os demais status
+            ->get();
+
+        // Agrupa os leads por status
+        return $leads->groupBy('status');
+
+
+        $leads = (new Leads)->dadosCard()
+            ->when($setor, function ($query) use ($setor) {
+                return $query->where('setor_id', $setor);
+            })
+            ->when($usuario, function ($query) use ($usuario) {
+                return $query->where('user_id', $usuario);
+            })
 //            ->whereIn('user_id', [id_usuario_atual()])
             ->orderBy('contato_data', 'asc')
             ->get();
