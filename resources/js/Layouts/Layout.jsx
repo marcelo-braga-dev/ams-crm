a54de2a879862d41dc4016609dadad1b5b0f2397
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-10-15 08:47:47 -0300

    fix lead telefones

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,44 +1,47 @@
-import React, {useState} from "react";
-import {Box, Container} from "@mui/material";
-import {Head, usePage} from "@inertiajs/react";
+import React, { useState } from 'react';
+import { Box, Container } from '@mui/material';
+import { Head, usePage } from '@inertiajs/react';
 
-import Header from './Header/Index'
-import Drawer from './Drawer/Index'
-import Content from './Content/Box'
-import ModalsAlerts from "@/Components/Modals/AlertsModals";
+import Header from './Header/Index';
+import Drawer from './Drawer/Index';
+import Content from './Content/Box';
+import ModalsAlerts from '@/Components/Modals/AlertsModals';
 
-import {AuthProvider} from './Contexts/Context';
-import ChatNotificacoes from "@/Components/Alerts/ChatNotificacoes.jsx";
+import { AuthProvider } from './Contexts/Context';
+import ChatNotificacoes from '@/Components/Alerts/ChatNotificacoes.jsx';
 import FabWhatsapp from '@/Components/Chats/Whatsapp/Fab/FabWhatsapp.jsx';
+import { WhatsappProvider } from '@/Contexts/WhatsappContext.jsx';
 
-export default function Layout({children, titlePage, voltar, menu, submenu}) {
-    const {app_settings} = usePage().props;
+export default function Layout({ children, titlePage, voltar, menu, submenu }) {
+    const { app_settings } = usePage().props;
 
     const _toggleMenu = sessionStorage.getItem('_toggleMenu') === 'true';
-    const [toggleMenu, setToggleMenu] = useState(!_toggleMenu)
+    const [toggleMenu, setToggleMenu] = useState(!_toggleMenu);
 
     const menuToggle = (value) => {
-        setToggleMenu(value)
-    }
+        setToggleMenu(value);
+    };
 
-    document.body.style.backgroundColor = app_settings.bg_color
+    document.body.style.backgroundColor = app_settings.bg_color;
 
     return (
-        <Box sx={{display: 'flex', width: '100%'}}>
-            <Head title={`${titlePage} - ${app_settings.app_name}`}/>
-            <ModalsAlerts/>
+        <Box sx={{ display: 'flex', width: '100%' }}>
+            <Head title={`${titlePage} - ${app_settings.app_name}`} />
+            <ModalsAlerts />
             <AuthProvider menu={menu} submenu={submenu} menuToggle={menuToggle} toggleMenu={toggleMenu}>
-                <ChatNotificacoes/>
-                <Header titlePage={titlePage} voltar={voltar}/>
-                <Drawer _toggleMenu={_toggleMenu}/>
+                <ChatNotificacoes />
+                <Header titlePage={titlePage} voltar={voltar} />
+                <Drawer _toggleMenu={_toggleMenu} />
 
                 <Content open={toggleMenu}>
                     <Container maxWidth={false}>
                         {children}
-                        <FabWhatsapp/>
+                        <WhatsappProvider>
+                            <FabWhatsapp />
+                        </WhatsappProvider>
                     </Container>
                 </Content>
             </AuthProvider>
         </Box>
-    )
+    );
 }
