ddcaf189038d162db3ed451aa8d5507a1cf9df44
Marcelo Braga <marcelobraga.dev@gmail.com>
2024-10-22 16:00:45 -0300

    change chat-interno

--- database/migrations/2024_05_29_172755_create_leads_status_historicos_table.php
+++ database/migrations/2024_05_29_172755_create_leads_status_historicos_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('leads_status_historicos', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('lead_id');
+            $table->unsignedBigInteger('lead_id')->index();
             $table->string('status');
+            $table->string('anotacoes')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
