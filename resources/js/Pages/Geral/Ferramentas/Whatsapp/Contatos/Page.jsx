b02fcf6f0eb88d8ccde5973d8e1b93ad3b288769
MARCELO BRAGA <marcelobraga.dev@gmail.com>
2024-10-31 21:01:11 -0300

    add contatos whatsapp

--- resources/js/Pages/Geral/Ferramentas/Whatsapp/Contatos/Page.jsx
+++ resources/js/Pages/Geral/Ferramentas/Whatsapp/Contatos/Page.jsx
@@ -4,49 +4,90 @@ import {useWhatsapp} from "@/Hooks/useWhatsapp.jsx";
 import CardContainer from "@/Components/Cards/CardContainer.jsx";
 import CardTitle from "@/Components/Cards/CardTitle.jsx";
 import CardBody from "@/Components/Cards/CardBody.jsx";
-import {Divider, Stack, Typography} from "@mui/material";
+import {Button, Stack, Typography, TextField, Badge, Grid} from "@mui/material";
 import CampoTexto from "@/Components/CampoTexto.jsx";
 import Avatar from "@mui/material/Avatar";
-import {TbBrandWhatsapp, TbChecks, TbHeadset, TbMessageCircle, TbUser} from "react-icons/tb";
+import {TbBrandWhatsapp, TbChecks, TbHeadset, TbMessageCircle, TbUser, TbUsers} from "react-icons/tb";
+import Chip from "@mui/material/Chip";
 
 const Page = () => {
     const {get} = useWhatsapp();
 
-    const [contatos, setContatos] = useState([])
+    const [contatos, setContatos] = useState([]);
+    const [filteredContatos, setFilteredContatos] = useState([]);
+    const [qtdPage, setQtdPage] = useState(20);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         const fetchContatos = async () => {
             const response = await get('contact');
-            console.log(response);
-            setContatos(response ?? [])
-        }
-        fetchContatos()
-    }, []);
+            setContatos(response ?? []);
+            setFilteredContatos(response ?? []);
+        };
+        fetchContatos();
+    }, [get]);
+
+    const handleQtdPage = () => {
+        setQtdPage(qtdPage + 2);
+    };
+
+    const handleSearch = (event) => {
+        const term = event.target.value.replace(/\D/g, '');
+        setSearchTerm(term);
+
+        const filtered = contatos.filter(contact =>
+            contact.number.includes(term)
+        );
+        setFilteredContatos(filtered);
+    };
 
     return (
         <Layout titlePage="Contatos do Whatsapp" menu="whatsapp" submenu="whatsapp-contatos">
             <CardContainer>
-                <CardTitle title="Contatos do Whatsapp"/>
-                <CardBody>
-                    {contatos.map(item => (
-                        <CardContainer>
-                            <CardBody>
-                                <Stack direction="row" spacing={2}>
-                                    <Avatar src={item.profilePicUrl} sx={{width: 60, height: 60}}/>
-                                    <Stack sx={{width: '100%'}}>
-                                        <CampoTexto titulo="Nome" texto={item.name} icone={TbUser}/>
-                                        <CampoTexto titulo="Número" texto={item.number} icone={TbBrandWhatsapp}/>
-                                        <CampoTexto titulo="Atendente" texto={item.tickets?.[0]?.user?.name ?? '-'} icone={TbHeadset}/>
+                <CardTitle
+                    title="Contatos do Whatsapp"
+                    icon={<TbUsers size={20}/>}
+                    children={
+                        <TextField
+                            label="Pesquisar pelo número"
+                            variant="outlined"
+                            fullWidth
+                            onChange={handleSearch}
+                        />}/>
+            </CardContainer>
 
-                                        {item.tickets?.map(ticket => ( <CardContainer>
+            {filteredContatos.map((item, index) => (
+                index < qtdPage && (
+                    <CardContainer key={index}>
+                        <CardBody>
+                            <Stack direction="row" spacing={2}>
+                                <Avatar src={item.profilePicUrl} sx={{width: 60, height: 60}}/>
+                                <Stack sx={{width: '100%'}}>
+                                    <CampoTexto titulo="Nome" texto={item.name} icone={TbUser}/>
+                                    <CampoTexto titulo="Número" texto={item.number} icone={TbBrandWhatsapp}/>
+                                    <CampoTexto titulo="Atendente" texto={item.tickets?.[0]?.user?.name ?? '-'} icone={TbHeadset}/>
+
+                                    <Grid container>
+                                        {item.extraInfo.map(info => (
+                                            <Grid>
+                                                <Chip label={`${info.name}: ${info.value}`} size="small" sx={{margin: 1}}/>
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+
+
+                                    {item.tickets?.map((ticket, ticketIndex) => (
+                                        <CardContainer key={ticketIndex}>
                                             <CardBody>
                                                 <div className="row">
                                                     <div className="col-auto">
-                                                        <TbMessageCircle size={25}/>
+                                                        <Badge badgeContent={ticket.unreadMessages} color="success">
+                                                            <TbMessageCircle size={25}/>
+                                                        </Badge>
+
                                                     </div>
                                                     <div className="col">
                                                         <Stack direction="row" spacing={1} alignItems="center">
-                                                            <TbChecks size={20} color={ticket.unreadMessages ? 'gray' : '#35bde7'}/>
                                                             <Typography>{ticket.lastMessage ?? '-'}</Typography>
                                                         </Stack>
                                                     </div>
@@ -55,21 +96,22 @@ const Page = () => {
                                                     </div>
                                                 </div>
                                             </CardBody>
-                                        </CardContainer>))}
-
-                                        {/*<Typography variant="body2">{item.createdAt}</Typography>*/}
-                                        <Stack>
-
-                                        </Stack>
-                                    </Stack>
+                                        </CardContainer>
+                                    ))}
                                 </Stack>
+                            </Stack>
+                        </CardBody>
+                    </CardContainer>
+                )
+            ))}
 
-                            </CardBody>
-                        </CardContainer>
-                    ))}
-                </CardBody>
-            </CardContainer>
+            {filteredContatos.length > qtdPage && (
+                <Stack sx={{alignItems: 'center', margin: 5}}>
+                    <Button onClick={handleQtdPage}>Ver Mais</Button>
+                </Stack>
+            )}
         </Layout>
-    )
-}
-export default Page
+    );
+};
+
+export default Page;
