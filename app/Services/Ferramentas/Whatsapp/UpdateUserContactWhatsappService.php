f614317c92631c8c88c781d85991658c6ad13c2e
MARCELO <marcelobraga.dev@gmail.com>
2024-11-21 14:24:28 -0300

    change lead

--- app/Services/Ferramentas/Whatsapp/UpdateUserContactWhatsappService.php
+++ app/Services/Ferramentas/Whatsapp/UpdateUserContactWhatsappService.php
@@ -4,58 +4,43 @@ namespace App\Services\Ferramentas\Whatsapp;
 
 use App\Models\Ferramentas\Whatsapp\WhatsappUsuario;
 use App\Models\Lead\LeadTelefones;
-use Exception;
 use Illuminate\Support\Facades\Http;
 
 class UpdateUserContactWhatsappService
 {
-    function optionsFetch($token)
-    {
-        return [
-            'headers' => [
-                'Authorization' => "Bearer {$token}",
-                'Content-Type' => 'application/json',
-            ],
-            'json' => [
-                'userId' => 4,
-                'contactId' => 27,
-            ],
-        ];
-    }
-
     public function update($leadId, $userId = null)
     {
+
         $settings = (new SettingsWatsappService())->settings();
 
         if (empty($settings['apiKey'])) {
-            throw new \InvalidArgumentException('API key is missing in settings.');
+            return;
         }
 
-        // Obtenha o ID do WhatsApp associado ao usuário
         $whatsappUsuario = (new WhatsappUsuario())
             ->where('user_id', $userId)
             ->first('whatsapp_id');
-        $whatsappId = $whatsappUsuario ? $whatsappUsuario->whatsapp_id : null;
 
-        if (is_null($whatsappId)) {
-            $whatsappId = null;
-        }
+        $whatsappId = $whatsappUsuario?->whatsapp_id;
+
+//        if (is_null($whatsappId)) {
+//            $whatsappId = null;
+//        }
 
         $telefones = (new LeadTelefones())
             ->where('lead_id', $leadId)
             ->whereNotNull('whatsapp_id')
-            ->get(['whatsapp_id']);
+            ->get();
 
         // Verifica se há telefones para processar
         if ($telefones->isEmpty()) {
-            throw new \DomainException("No WhatsApp IDs found for lead ID: $leadId");
+            return;
         }
 
         $url = "{$settings['urlBackend']}/api/contacts/update-user";
 
         try {
             foreach ($telefones as $contato) {
-                // Realiza a chamada HTTP para cada telefone
                 Http::accept('application/json')
                     ->withToken($settings['apiKey'])
                     ->post($url, [
@@ -64,7 +49,7 @@ class UpdateUserContactWhatsappService
                     ]);
             }
         } catch (\Exception $e) {
-            throw new \DomainException("Failed to update user contacts at $url: {$e->getMessage()}");
+            throw new \DomainException("$url: {$e->getMessage()}");
         }
     }
 
