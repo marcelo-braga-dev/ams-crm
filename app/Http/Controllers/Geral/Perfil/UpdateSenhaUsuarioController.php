1e65c427e3a3c799835a71b318b604adbb36a7f2
MARCELO <marcelobraga.dev@gmail.com>
2024-11-25 15:22:48 -0300

    change lead

--- app/Http/Controllers/Geral/Perfil/UpdateSenhaUsuarioController.php
+++ app/Http/Controllers/Geral/Perfil/UpdateSenhaUsuarioController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Geral\Perfil;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UpdateSenhaUsuarioController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $userId = $request->user_id ?? id_usuario_atual();
+
         if ($request->get('nova_senha') !== $request->get('confirmar_senha')) {
             modalErro('Nova senha e Confirmar Senha não coincidem!');
             return redirect()->back();
@@ -20,7 +22,7 @@ class UpdateSenhaUsuarioController extends Controller
             'nova_senha' => 'required|string|min:4',
         ], ['nova_senha.min' => 'A senha deve ter no mínimo 4 dígitos']);
 
-        $user = Auth::user();
+        $user = (new User)->find($userId);
         $user->password = Hash::make($request->get('nova_senha'));
         $user->save();
 
--- resources/js/Pages/Admin/Usuarios/Index.jsx
+++ resources/js/Pages/Admin/Usuarios/Index.jsx
@@ -2,15 +2,24 @@ import Layout from '@/Layouts/Layout';
 import * as React from 'react';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import BlockIcon from '@mui/icons-material/Block';
-import {Avatar, Grid, Stack} from "@mui/material";
+import {Avatar, Button, DialogContent, Grid, IconButton, Stack, TextField, Typography} from "@mui/material";
 import {router} from "@inertiajs/react";
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import CardContainer from "@/Components/Cards/CardContainer";
 import CardBody from "@/Components/Cards/CardBody";
 import Link from "@/Components/Link.jsx";
+import Dialog from "@mui/material/Dialog";
+import {useState} from "react";
+import {TbEye, TbKey} from "react-icons/tb";
+import AlertError from "@/Components/Alerts/AlertError.jsx";
 
 export default function Index({contas, status}) {
+    const [openDialog, setOpenDialog] = useState(false)
+
+    const [userId, setUserId] = useState('')
+    const [senha, setSenha] = useState('')
+    const [senhaConfirmar, setSenhaConfirmar] = useState('')
 
     function iconeStatus(status) {
         return status ? <CheckCircleOutlineIcon color="success" sx={{fontSize: 16}}/> :
@@ -19,6 +28,35 @@ export default function Index({contas, status}) {
 
     function escolherStatus(status) {
         router.get(route('admin.usuarios.usuario.index', {status: status}))
+    }
+
+    const handleOpenDialog = (userId) => {
+        console.log(userId)
+        setUserId(userId)
+        setOpenDialog(true)
+    }
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false)
+    }
+
+    const alterarSenha = () => {
+        if (senha.length < 4) {
+            AlertError("A senha deve ter no mínimo 4 dígitos.")
+            return
+        }
+
+        if (senha !== senhaConfirmar) {
+            AlertError("Senha e Repetir Senha não coincidem.")
+            return
+        }
+
+        router.post(route('auth.perfil.atualizar-senha'), {
+            user_id: userId,
+            nova_senha: senha,
+            confirmar_senha: senhaConfirmar,
+            _method: 'put'
+        }, {onSuccess: () => handleCloseDialog()})
     }
 
     return (
@@ -30,12 +68,12 @@ export default function Index({contas, status}) {
                 <div className="col-auto">
                     <FormControlLabel control={
                         <Switch defaultChecked={status}
-                            onChange={e => escolherStatus(e.target.checked)}/>} label="Mostrar Bloqueados"/>
+                                onChange={e => escolherStatus(e.target.checked)}/>} label="Mostrar Bloqueados"/>
                 </div>
             </div>
             {contas.map((item) => (
                 <CardContainer>
-                    <CardBody className="cursor-pointer" onClick={() => router.get(route('admin.usuarios.usuario.show', item.id))}>
+                    <CardBody>
                         <div className="row">
                             <div className="col-auto">
                                 <Avatar src={'/storage/' + item.foto} sx={{width: 40, height: 40}}/>
@@ -63,11 +101,41 @@ export default function Index({contas, status}) {
                                 </div>
                             </div>
                             <div className="col-auto">
-                                <button className="btn btn-primary btn-sm mb-0 mt-2">Ver</button>
+                                <Stack spacing={2}>
+                                    <IconButton
+                                        onClick={() => router.get(route('admin.usuarios.usuario.show', item.id))}>
+                                        <TbEye color="blue"/>
+                                    </IconButton>
+                                    <IconButton onClick={() => handleOpenDialog(item.id)}><TbKey color="orange"/></IconButton>
+                                </Stack>
                             </div>
                         </div>
                     </CardBody>
                 </CardContainer>
             ))}
+            <Dialog open={openDialog}
+                    onClose={handleCloseDialog}
+                    fullWidth
+                    maxWidth="xs"
+            >
+                <DialogContent>
+                    <Stack spacing={2}>
+                        <Typography variant="h5">Alterar Senha</Typography>
+                        <TextField
+                            label="Nova Senha"
+                            type="password"
+                            fullWidth
+                            onChange={e => setSenha(e.target.value)}
+                        />
+                        <TextField
+                            label="Repetir Senha"
+                            type="password"
+                            fullWidth
+                            onChange={e => setSenhaConfirmar(e.target.value)}
+                        />
+                        <Button color="success" onClick={alterarSenha}>Alterar Senha</Button>
+                    </Stack>
+                </DialogContent>
+            </Dialog>
         </Layout>);
 }
