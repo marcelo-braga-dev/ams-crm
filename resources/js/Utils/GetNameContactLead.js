466a2d836797b1dfdd86a79822819dbbec6b541c
MARCELO <marcelobraga.dev@gmail.com>
2024-12-13 08:45:26 -0300

    change lead

--- resources/js/Utils/GetNameContactLead.js
+++ resources/js/Utils/GetNameContactLead.js
@@ -4,7 +4,7 @@ const getLead = async (leadId) => {
     try {
         const {data} = await axios.get(route('auth.lead.find-lead', leadId));
         console.log(data)
-        return {contato_nome: `${data.nome.trim() ?? data.razao_social} [ID #${leadId}]`, ...data.whatsapp}
+        return {contato_nome: `${data?.nome?.trim() ?? data.razao_social} [ID #${leadId}]`, ...data.whatsapp}
     } catch (error) {
         console.error("Erro ao buscar os dados do lead:", error);
         throw error;
