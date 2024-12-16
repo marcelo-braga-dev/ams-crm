4eac9a21e6ca7b711cb663896b19bcc00ff06de2
MARCELO <marcelobraga.dev@gmail.com>
2024-12-16 11:25:49 -0300

    change lead

--- app/Http/Controllers/Geral/Leads/Api/UpdateFinalizarLeadController.php
+++ app/Http/Controllers/Geral/Leads/Api/UpdateFinalizarLeadController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Geral\Leads\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\Ferramentas\Whatsapp\UpdateUserContactWhatsappService;
 use App\src\Leads\StatusLeads\FinalizadoStatusLead;
 use Illuminate\Http\Request;
 
-class SetFinalizarLeadController extends Controller
+class UpdateFinalizarLeadController extends Controller
 {
     public function __invoke(Request $request)
     {
         (new FinalizadoStatusLead())->updateStatus($request->lead_id, $request->motivo);
+        (new UpdateUserContactWhatsappService())->update($request->lead_id);
 
         modalSucesso('Lead finalizado com sucesso!');
     }
--- routes/auth/leads/lead-api.php
+++ routes/auth/leads/lead-api.php
@@ -1,14 +1,14 @@
 <?php
 
 use App\Http\Controllers\Geral\Leads\Api\EncaminharLeadController;
-use App\Http\Controllers\Geral\Leads\Api\SetFinalizarLeadController;
+use App\Http\Controllers\Geral\Leads\Api\UpdateFinalizarLeadController;
 use App\Http\Controllers\Geral\Leads\Api\UpdateContatoLeadController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('auth.leads.api.')
     ->prefix('auth/leads-api')
     ->group(function () {
-        Route::post('finalizar', SetFinalizarLeadController::class)->name('finalizar');
+        Route::post('finalizar', UpdateFinalizarLeadController::class)->name('finalizar');
         Route::post('encaminhar', EncaminharLeadController::class)->name('encaminhar');
 
         Route::post('update-contato', UpdateContatoLeadController::class)->name('update-contato');
