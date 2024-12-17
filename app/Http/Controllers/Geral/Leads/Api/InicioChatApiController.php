b1b87139abb62b199412518b1a21d39fbfe44ab6
MARCELO <marcelobraga.dev@gmail.com>
2024-12-17 16:30:43 -0300

    fix planilha

--- app/Http/Controllers/Geral/Leads/Api/InicioChatApiController.php
+++ app/Http/Controllers/Geral/Leads/Api/InicioChatApiController.php
@@ -11,20 +11,22 @@ use Illuminate\Http\Request;
 
 class InicioChatApiController extends Controller
 {
-   public function __invoke(Request $request)
-   {
-       $leadId = $request->input('lead_id');
-       $telefoneId = $request->input('telefone_id');
-       $origem = $request->input('origem');
-       $meta = $request->input('meta');
+    public function __invoke(Request $request)
+    {
+        $leadId = $request->input('lead_id');
+        $telefoneId = $request->input('telefone_id');
+        $origem = $request->input('origem');
+        $meta = $request->input('meta');
 
-       $lead = (new Lead())->newQuery()->find($leadId);
-       $lead->update(['contato_data' => now()]);
-       if ($lead->status === 'novo' || $lead->status === 'oportunidade') {
-           (new UpdateStatusLeadService($leadId))->setConexaoProativaStatus($leadId);
-           $lead->update(['status_data' => now()]);
-       }
+        $lead = (new Lead())->newQuery()->find($leadId);
 
-       (new LeadContatoRealizado())->store($leadId, $telefoneId, $origem, $meta);
-   }
+        $lead->update(['contato_data' => now()]);
+
+        if ($lead->status === 'novo' || $lead->status === 'oportunidade') {
+            (new UpdateStatusLeadService($leadId))->setConexaoProativaStatus($leadId);
+            $lead->update(['status_data' => now()]);
+        }
+
+        (new LeadContatoRealizado())->store($leadId, $telefoneId, $origem, $meta);
+    }
 }
