afe23365344a721767bd47a0c9627fd397795a28
MARCELO <marcelobraga.dev@gmail.com>
2025-01-03 14:30:37 -0300

    edit voip

--- resources/js/Pages/Consultor/Pedidos/Faturado/Show.jsx
+++ resources/js/Pages/Consultor/Pedidos/Faturado/Show.jsx
@@ -1,34 +1,41 @@
 import Layout from "@/Layouts/Layout";
-import { router } from '@inertiajs/react'
+import {router} from '@inertiajs/react'
 
 import React from 'react';
 import {useForm} from '@inertiajs/react';
 import ImagePdf from "@/Components/Elementos/ImagePdf";
 import DadosPedido from "@/Components/Pedidos/DadosPedido";
 import DadosPedidoCliente from "@/Components/Pedidos/DadosPedidoCliente";
+import CardContainer from "@/Components/Cards/CardContainer.jsx";
+import CardBody from "@/Components/Cards/CardBody.jsx";
 
 export default function Create({pedido}) {
-
-    const atualizarStatus = () => {
-        router.post(route('consultor.pedidos.atualizar-status'), {id: pedido.id})
-    }
-
     return (
-        <Layout container voltar={route('consultor.pedidos.index', {id_card:  pedido.pedido.id})} titlePage="Pedido Faturado" >
+        <Layout container voltar={route('consultor.pedidos.index', {id_card: pedido.pedido.id})} titlePage="Pedido Faturado">
             <div className="row mb-4">
                 <div className="col mb-4">
-                    <DadosPedido dados={pedido}/>
+                    <CardContainer>
+                        <CardBody>
+                            <DadosPedido dados={pedido}/>
+                        </CardBody>
+                    </CardContainer>
                 </div>
                 <div className="col mb-4">
-                    <DadosPedidoCliente dados={pedido}/>
+                    <CardContainer>
+                        <CardBody>
+                            <DadosPedidoCliente dados={pedido}/>
+                        </CardBody>
+                    </CardContainer>
                 </div>
             </div>
             <div className="row">
                 <div className="col mb-4">
-                    <h6>Baixar Nota/Boleto</h6>
-                    <div onClick={atualizarStatus}>
-                        <ImagePdf url={pedido.pedido_files.nota_fiscal}/>
-                    </div>
+                    <CardContainer>
+                        <CardBody>
+                            <h6>Baixar Nota/Boleto</h6>
+                            <ImagePdf url={pedido.pedido_files.nota_fiscal}/>
+                        </CardBody>
+                    </CardContainer>
                 </div>
             </div>
         </Layout>
